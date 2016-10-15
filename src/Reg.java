1cd2572314f0c3ef21d7502b93f78072cd10708d
Resspectine <resspectyn@gmail.com>
2016-10-15 14:17:43 +0300

    Начало положено

--- src/Reg.java
+++ src/Reg.java
@@ -1,9 +1,21 @@
-/**
- * Created by User on 15.10.2016.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class Reg {
-    public static void main(String [] args)
-    {
-        
+    public static boolean checkWithRegExp(String userNameString){
+        Pattern p = Pattern.compile("\\+?[375,801]");
+        Matcher m = p.matcher(userNameString);
+        return m.matches();
+    }
+    public static void main(String[] args) {
+        String input;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество номеров: ");
+        input = sc.nextLine();
+        String [] telephoneNumbers=new String[Integer.valueOf(input)];
+        input = sc.nextLine();
+        telephoneNumbers[0]=input;
+
     }
 }
